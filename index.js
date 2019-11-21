55ea7af1217134627dfe327527fa9d669da8a7b7
CHidave291 <dukelere@gmail.com>
2019-11-21 13:36:43 +0100

    Completed setup transpile

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/', (request, response) => {
+  response.status(404).send({
+    status: 404,
+    error: 'Not Found !',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Visit port loaclhost:${PORT}`);
+});
